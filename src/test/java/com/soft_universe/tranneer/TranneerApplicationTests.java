779a7aa881d239de492892f44fe146c978e36c8e
heshan <heshansandakelum2018@gmail.com>
2026-07-04 03:54:17 +0530

    initialized the project

---
+++ src/test/java/com/soft_universe/tranneer/TranneerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft_universe.tranneer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranneerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
