604c681bd6d50255133de4c40ef58ce433a8b784
heshan <heshansandakelum2018@gmail.com>
2026-07-04 03:54:17 +0530

    initialized the project

---
+++ src/main/java/com/soft_universe/tranneer/TranneerApplication.java
@@ -0,0 +1,13 @@
+package com.soft_universe.tranneer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranneerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranneerApplication.class, args);
+	}
+
+}
