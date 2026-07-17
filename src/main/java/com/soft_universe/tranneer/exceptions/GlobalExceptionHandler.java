e46a26def1e8e3002f450f55cca83d684c7d554d
heshan <heshansandakelum2018@gmail.com>
2026-07-17 13:26:17 +0530

    update global exception handler

--- src/main/java/com/soft_universe/tranneer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/soft_universe/tranneer/exceptions/GlobalExceptionHandler.java
@@ -2,20 +2,31 @@ package com.soft_universe.tranneer.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //for aliens
     @ExceptionHandler(AlienNotFoundException.class)
     public ResponseEntity <ErrorResponceDTO> handleAlienNotFound(AlienNotFoundException ex){
         ErrorResponceDTO error=new ErrorResponceDTO(
                 ex.getMessage(),
                 HttpStatus.NOT_FOUND.value(),
                 LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+    //for planet
+    @ExceptionHandler(PlanetNotFoundException.class)
+    public ResponseEntity<ErrorResponceDTO> handlePlanetNotFound(PlanetNotFoundException ex){
+        ErrorResponceDTO error =new ErrorResponceDTO(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+
         );
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
     }
