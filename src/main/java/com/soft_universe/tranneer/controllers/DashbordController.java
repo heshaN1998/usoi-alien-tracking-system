069cc06768f13d0e0c78c5198b7e81fb2a6e4267
heshan <heshansandakelum2018@gmail.com>
2026-07-18 10:52:45 +0530

    adding Dashbord Controller

---
+++ src/main/java/com/soft_universe/tranneer/controllers/DashbordController.java
@@ -0,0 +1,53 @@
+package com.soft_universe.tranneer.controllers;
+
+import com.soft_universe.tranneer.dtos.DasbordDTOs.*;
+import com.soft_universe.tranneer.services.DashbordService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api01/dashbord")
+@CrossOrigin
+public class DashbordController {
+
+    private final DashbordService dashbordService;
+
+    public DashbordController(DashbordService dashbordService){
+        this.dashbordService = dashbordService;
+
+    }
+
+
+    @GetMapping("/summary")
+    public DashbordSummeryDTO summary(){
+        return dashbordService.getSummery();
+    }
+
+
+    @GetMapping("/statistics")
+    public DashbordStatisticsDTO statistics(){
+        return dashbordService.getStatistics();
+    }
+
+
+    @GetMapping("/charts")
+    public List<DashbordChartDTO> charts(){
+        return dashbordService.getChart();
+    }
+
+
+    @GetMapping("/alien-statistics")
+    public AlienStatisticDTO alienStatistics(){
+        return dashbordService.getAlienStatistics();
+    }
+
+
+    @GetMapping("/planet-statistics")
+    public List<PlanetStatisticDTO> planetStatistics(){
+        return dashbordService.getPlanetStatistics();
+    }
+
+}
