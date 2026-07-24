446f3369c0403f1e6d9c2d6273e1e6651c0a567c
heshan <heshansandakelum2018@gmail.com>
2026-07-25 03:38:25 +0530

    fixing application prop and completed testing part in postman except ai section

--- src/main/java/com/soft_universe/tranneer/ai/service/AICommanderServiceIMPL.java
+++ src/main/java/com/soft_universe/tranneer/ai/service/AICommanderServiceIMPL.java
@@ -1,30 +1,21 @@
 package com.soft_universe.tranneer.ai.service;
 
-import com.soft_universe.tranneer.ai.agents.predictAgent.PredictionAgent;
-import com.soft_universe.tranneer.ai.agents.researchAgent.ResearchAgent;
-import com.soft_universe.tranneer.ai.agents.securityAgent.SecurityAgent;
 import com.soft_universe.tranneer.ai.dtos.CommanderResponseDTO;
 import com.soft_universe.tranneer.ai.memory.PersistentMemoryService;
 import com.soft_universe.tranneer.ai.prompt.CommanderPromptBuilder;
 import com.soft_universe.tranneer.ai.rag.CommanderKnowledgeService;
-import com.soft_universe.tranneer.ai.rag.KnowledgeRetriever;
 import com.soft_universe.tranneer.ai.tools.PredictTools;
 import com.soft_universe.tranneer.ai.tools.ReasearchTools;
 import com.soft_universe.tranneer.ai.tools.SecureTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.messages.AssistantMessage;
-import org.springframework.ai.chat.messages.Message;
 
-import java.util.List;
 
 
 @Service
 public class AICommanderServiceIMPL implements AICommanderService {
     private final ChatClient chatClient;
-    private final KnowledgeRetriever retriever;
     private final CommanderPromptBuilder promptBuilder;
 
     private final SecureTools secureTools;
@@ -35,7 +26,6 @@ public class AICommanderServiceIMPL implements AICommanderService {
     private final PersistentMemoryService memoryService;
 
     public AICommanderServiceIMPL(ChatClient chatClient,
-                                  KnowledgeRetriever retriever,
                                   CommanderPromptBuilder promptBuilder,
                                   SecureTools secureTools,
                                   ReasearchTools reasearchTools,
@@ -45,7 +35,6 @@ public class AICommanderServiceIMPL implements AICommanderService {
                                   PersistentMemoryService memoryService
     ) {
         this.chatClient = chatClient;
-        this.retriever = retriever;
         this.promptBuilder = promptBuilder;
         this.secureTools = secureTools;
         this.predictTools = predictTools;
--- src/main/java/com/soft_universe/tranneer/controllers/DashbordController.java
+++ src/main/java/com/soft_universe/tranneer/controllers/DashbordController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api01/dashbord")
+@RequestMapping("/api/dashbord")
 @CrossOrigin
 public class DashbordController {
 
